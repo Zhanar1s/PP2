5e2dec1427cc04e1d8a9d0a32cd63529c231a4a2
Akmurzayev Zhanarys <zhanarys.akmurzaev@gmail.com>
2024-01-18 22:57:47 +0600

    |

---
+++ exercises.py
@@ -0,0 +1,90 @@
+ #1.1 
+print("Hello World")
+
+ #1.2 
+if 5 > 2:
+ print("YES")
+
+ #2.1 
+#This is a comment
+
+ #2.2
+"""This is a comment
+written in 
+more than just one line"""
+
+ #3.1
+carname = "Volvo"
+ 
+ #3.2
+ x = 50
+
+ #3.3
+x = 5
+y = 10
+print(x + y)
+
+ #3.4
+x = 5
+y = 10
+z = x + y
+print(z)
+
+ #3.5
+x, y, z = "Orange", "Banana", "Cherry"
+
+ #3.6
+x = y = z = "Orange"
+
+ #3.7
+def myfunc():
+  global x
+  x = "fantastic"
+
+ #4.1
+x = 5
+print(type(x))
+int
+
+ #4.2
+x = "Hello World"
+print(type(x))
+str
+
+ #4.3
+x = 20.5
+print(type(x))
+float
+
+ #4.4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+list
+
+ #4.5
+x = ("apple", "banana", "cherry")
+print(type(x))
+tuple
+
+ #4.6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+dict
+
+ #4.7
+x = True
+print(type(x))
+bool
+
+ #5.1
+x = 5
+x = float(x)
+
+ #5.2
+x = 5.5
+x = int(x)
+
+ #5.3
+
+
+

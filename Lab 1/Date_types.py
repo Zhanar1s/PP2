31775c84d24ed6b68449bf83a679cc9b7c564958
Akmurzayev Zhanarys <zhanarys.akmurzaev@gmail.com>
2024-01-21 02:59:28 +0600

    LAB1

---
+++ Lab 1/Date_types.py
@@ -0,0 +1,40 @@
+#1. The following code example would print the data type of x, what data type would that be?
+x = 5
+print(type(x))
+Answer: int
+
+
+#2. The following code example would print the data type of x, what data type would that be?
+x = "Hello World"
+print(type(x))
+Answer: str
+
+
+#3. The following code example would print the data type of x, what data type would that be?
+x = 20.5
+print(type(x))
+Answer: float
+
+
+#4. The following code example would print the data type of x, what data type would that be?
+x = ["apple", "banana", "cherry"]
+print(type(x))
+Answer: list
+
+
+#5. The following code example would print the data type of x, what data type would that be?
+x = ("apple", "banana", "cherry")
+print(type(x))
+Answer: tuple
+
+
+#6. The following code example would print the data type of x, what data type would that be?
+x = {"name" : "John", "age" : 36}
+print(type(x))
+Answer: dict
+
+
+#7. The following code example would print the data type of x, what data type would that be?
+x = True
+print(type(x))
+Answer: bool

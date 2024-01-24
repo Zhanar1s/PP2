15e380f1e5c8e277e5c0e44d2ff379357c295678
Akmurzayev Zhanarys <zhanarys.akmurzaev@gmail.com>
2024-01-24 12:13:52 +0600

    week 2

---
+++ Lab 2/booleans.py
@@ -0,0 +1,16 @@
+#1. The statement below would print a Boolean value, which one?
+print(10 > 9)   #output: True
+
+
+#2. The statement below would print a Boolean value, which one?
+print(10 == 9)  #output: False
+
+
+#3. The statement below would print a Boolean value, which one?
+print(10 < 9)   #output: False
+
+#4. The statement below would print a Boolean value, which one?
+print(bool("abc"))  #output: True
+
+#5. The statement below would print a Boolean value, which one?
+print(bool(0))  #output: False

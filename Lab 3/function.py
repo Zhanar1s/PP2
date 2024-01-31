2c1a85e02e0edb30a0ad355c3801ef8a13d6d364
Akmurzayev Zhanarys <zhanarys.akmurzaev@gmail.com>
2024-01-31 09:41:51 +0600

    .

---
+++ Lab 3/function.py
@@ -0,0 +1,29 @@
+#1. Create a function named my_function.
+def my_function():
+    print("Hello from a function")
+
+
+#2. Execute a function named my_function.
+def my_function():
+  print("Hello from a function")
+my_function()
+
+
+#3. Inside a function with two parameters, print the first parameter.
+def my_function(fname, lname):
+  print(fname)
+
+
+#4. Let the function return the x parameter + 5.
+def my_function(x):
+    return x + 5
+
+
+#5. If you do not know the number of arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix?
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+
+#6. If you do not know the number of keyword arguments that will be passed into your function, there is a prefix you can add in the function definition, which prefix?
+def my_function(**kid):
+  print("His last name is " + kid["lname"])

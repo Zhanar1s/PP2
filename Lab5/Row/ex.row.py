5539e68a4ad7220561d0ede9879ee62f9af0a2ee
Akmurzayev Zhanarys <zhanarys.akmurzaev@gmail.com>
2024-02-28 09:42:02 +0600

    week 5

--- Lab5/Row/ex.row.py
+++ Lab5/Row/ex.row.py
@@ -1,10 +1,26 @@
-elements = ['реті\n', 'аты\n', 'саны\n', 'бағасы\n', 'кұны\n']
+import re
+import csv
+from tabulate import tabulate
 
-# Write to the file
-with open('row.txt', 'w', encoding='utf-8') as fw:
-    fw.writelines(elements)
+f = open('row.txt', 'r', encoding='utf8')
+text = f.read()
 
-# Read from the file
-with open('row.txt', 'r', encoding='utf-8') as fr:
-    for line in fr:
-        print(line, end='')
+pattern = r"\n(?P<реті>[0-9]+)\.\n(?P<аты>.+)\n(?P<саны>.+)x(?P<бағасы>.+)\n(?P<құны>.+)"
+
+res = re.finditer(pattern, text)
+
+data = []
+for x in res:
+    data.append([
+        x.group('реті'), 
+        x.group('аты'),
+        float(x.group('саны').strip().replace(',','.')),
+        float(x.group('бағасы').strip().replace(',','.').replace(' ','')),
+        float(x.group('құны').strip().replace(',','.').replace(' ',''))
+    ])
+
+headers = ['реті', 'аты', 'саны', 'бағасы', 'құны']
+table = tabulate(data, headers=headers, tablefmt='pretty')
+
+with open('formatted_row.txt', 'w', encoding='utf8') as txtfile:
+    txtfile.write(table)
